8c53d13208efb917f3629e73c377ddde8138d44b
Robert Lillack <rob@lillack.net>
2021-07-04 13:50:09 +0200

    Update for tack 1.2.0

--- Formula/tack.rb
+++ Formula/tack.rb
@@ -1,14 +1,14 @@
 class Tack < Formula
   desc "Sustainable static site generator"
   homepage "https://burningsoda.com/software/tack"
-  url "https://burningsoda.com/software/tack/download/tack-1.1.0.tar.gz"
-  sha256 "30efd5a25e889e0d3b4c3333baf147e9bedf1ab32967cbd284fc2026fd7b227a"
+  url "https://burningsoda.com/software/tack/download/tack-1.2.0.tar.gz"
+  sha256 "b67a8598f13e8581e27b5418195ec6f3452d8b65076fd972d4f6c64ace465477"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", "-ldflags", "-X github.com/roblillack/tack/commands.Version=v#{version}", *std_go_args
     # bin.install "tack"
     man.mkpath
     man1.install "tack.1"
